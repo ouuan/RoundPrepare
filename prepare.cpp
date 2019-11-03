efeb4cbc8204da0c67caf2306b09da152581a028
ouuan <y___o___u@126.com>
2019-11-03 16:50:59 +0800

    :bug::art: fix space issues & format indents

--- prepare.cpp
+++ prepare.cpp
@@ -9,46 +9,48 @@ string problem_path, template_path;
 
 string quote(string s)
 {
-	return "\"" + s + "\"";
+    return "\"" + s + "\"";
 }
 
 void copy_template(string file_name)
 {
-	system(("copy " + quote(template_path) + " " + quote(problem_path + "\\" + file_name)).c_str());
-	system(quote(problem_path + "\\" + file_name).c_str());
+    system(("copy " + quote(template_path) + " " + quote(problem_path + "\\" + file_name)).c_str());
+    system(("start \"\" " + quote(problem_path + "\\" + file_name)).c_str());
 }
 
 int main()
 {
-	ifstream fin("config.ini");
-	getline(fin, template_path);
-	
-	int tot;
-	
-	cout << "Site\\Round name: ";
-	getline(cin, problem_path);
-	system(("mkdir " + quote(problem_path)).c_str());
-	
-	cout << "Number of problems (-number for custom name): ";
-	cin >> tot;
-	
-	if (tot >= 0)
-	{
-		for (int i = 0; i < tot; ++i)
-		{
-			copy_template(string(1, 'A' + i) + ".cpp");
-		}
-	}
-	else
-	{
-		for (int i = 0; i > tot; --i)
-		{
-			string problem;
-			cout << "Problem " << - i + 1 << ": ";
-			cin >> problem;
-			copy_template(problem + ".cpp");
-		}
-	}
-	
-	return 0;
+    ifstream fin("config.ini");
+    getline(fin, template_path);
+
+    int tot;
+
+    cout << "Site\\Round name: ";
+    getline(cin, problem_path);
+    system(("mkdir " + quote(problem_path)).c_str());
+
+    cout << "Number of problems (-number for custom name): ";
+    string tmp;
+    getline(cin, tmp);
+    tot = atoi(tmp.c_str());
+
+    if (tot >= 0)
+    {
+        for (int i = 0; i < tot; ++i)
+        {
+            copy_template(string(1, 'A' + i) + ".cpp");
+        }
+    }
+    else
+    {
+        for (int i = 0; i > tot; --i)
+        {
+            string problem;
+            cout << "Problem " << - i + 1 << ": ";
+            getline(cin, problem); 
+            copy_template(problem + ".cpp");
+        }
+    }
+
+    return 0;
 }
